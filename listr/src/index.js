9a938c997b90578458e089c05c3d2937fbf9a655
x-zen <jdharris.x@gmail.com>
2019-03-08 03:23:49 -0600

    Set-up redux store

--- listr/src/index.js
+++ listr/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 /*Components*/
 import App from './App';
 /*Dependencies*/
-//import { createStore } from 'redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 //import thunk from 'redux-thunk';
 //import { BrowserRouter as Router } from 'react-router-dom';
 
+//Temp reducer function
+function reducer() {
+  return {
+    state: 'temp state'
+  }
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
