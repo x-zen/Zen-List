7d68bdca2916a67a7161f60991b7cc671e5e9726
x-zen <jdharris.x@gmail.com>
2019-03-08 05:35:33 -0600

    List title fully functional

--- listr/src/comp/List/comp/Title/Title.js
+++ listr/src/comp/List/comp/Title/Title.js
@@ -5,21 +5,38 @@ import { updateTitle } from '../../../../actions/index.js';
 
 class Title extends React.Component {
   state = {
-    newTitleText: ''
+    newTitleText: this.props.title,
+    editing: false
   };
 
   updateTitle = e => {
     e.preventDefault();
     this.props.updateTitle(this.state.newTitleText)
+    this.setState({editing: false})
+  }
+
+  handleChanges = e => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
-    return (
-      <div className='list-title'>
-        <h1>{this.props.initialState}</h1>
+
+    if (this.state.editing === false) {
+      return(<div className='list-title'>
+        <h1>{this.props.title}</h1>
+        <button onClick={() => this.setState({editing: true})}>Update</button>
+      </div>)
+    } else {
+      return(<div className='list-title'>
+        <input
+          type='text'
+          name='newTitleText'
+          value={this.state.newTitleText}
+          onChange={this.handleChanges}
+        />
         <button onClick={this.updateTitle}>Update</button>
-      </div>
-    )
+      </div>)
+    }
   }
 }
 
--- listr/src/index.js
+++ listr/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /*Dependencies*/
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-//import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 //import { BrowserRouter as Router } from 'react-router-dom';
+//import axios from 'axios';
 /*Reducer*/
 import rootReducer from './reducers/index.js';
 /*Components*/
 import App from './App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
